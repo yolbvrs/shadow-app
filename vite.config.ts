2157633fbabd82e07d1fc8cf86e777558341dce8
yolbvrs <myhreyolbars@gmail.com>
2025-06-27 15:55:15 +0800

    Fix Vercel build structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig({
+  plugins: [react()],
+  root: '.', // <- root should be current directory
+  build: {
+    outDir: 'dist',
   },
-  plugins: [
-    react()
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+})
