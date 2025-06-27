01916ea6ad70f22c5dbc995584f291274a54f689
yolbvrs <myhreyolbars@gmail.com>
2025-06-27 15:58:28 +0800

    Fix Vercel build path issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // <- root should be current directory
+  base: './',
   build: {
     outDir: 'dist',
   },
-})
+});
